8e5c315e075b9fd194f580e26586aff58020d93a
godfreydekew <dekewgodfrey@gmail.com>
2025-03-11 09:00:06 +0200

    Add getUserInformation endpoint

--- src/app/controllers/users/getUserInformation/index.js
+++ src/app/controllers/users/getUserInformation/index.js
@@ -4,14 +4,14 @@ const handleAPIError = require("~root/utils/handleAPIError");
 const getUserInformation = async (req, res) => {
   const { user_id } = req.params;
 
-  if (isNaN(user_id)) {
-    return handleAPIError(res, new Error("User id should be an integer"));
+  if (Number.isNaN(Number(user_id))) {
+    return handleAPIError(res, new Error("Invalid user ID format"), 400);
   }
 
   try {
     const { user } = await fetchUserInformation({ user_id });
 
-    res.status(200).send({
+    return res.status(200).send({
       user
     });
   } catch (err) {

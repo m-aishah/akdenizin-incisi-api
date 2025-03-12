422c6a32b51cf8bf5dda49aa31d7e2b53adcbfa1
Aishah <maishah2540@gmail.com>
2025-03-12 15:56:01 +0200

    update validation schema in postEvent to ensure proper typechecking for strings

--- src/app/controllers/events/postEvent/schemas/postEventSchema.js
+++ src/app/controllers/events/postEvent/schemas/postEventSchema.js
@@ -3,26 +3,31 @@ const yup = require("yup");
 const postEventSchema = yup.object().shape({
   title: yup
     .string()
+    .strict()
     .required("The title is required")
     .label("Title")
     .typeError("The title must be a string"),
   description: yup
     .string()
+    .strict()
     .required("The description is required")
     .label("Description")
     .typeError("The description must be a string"),
   date: yup
     .date()
+    .strict()
     .required("The date is required")
     .label("Date")
     .typeError("The date must be a valid date"),
   time: yup
     .string()
+    .strict()
     .required("The time is required")
     .label("Time")
     .typeError("The time must be a string"),
   location: yup
     .string()
+    .strict()
     .required("The location is required")
     .label("Location")
     .typeError("The location must be a string")
